6f3f978e669732b20e2233e0c7f74e0534870124
Matthew Macovsky <macovsky.m@gmail.com>
2014-11-24 19:17:40 -0800

    Added text files and arduino files

---
+++ sketch_aug16a/sketch_aug16a.ino
@@ -0,0 +1,33 @@
+char c;
+
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop() {
+  Serial.begin(9600);
+  c = Serial.read();
+  if (c=='0') {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+//    delay(1000);
+  } else if (c=='1') {
+    digitalWrite(2, LOW);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+ //   delay(1000);
+  } else if (c=='2') {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, HIGH);
+//    delay(1000);
+  } else {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);    
+  }
+  delay(1000);
+}
