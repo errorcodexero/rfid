eb33327c049e9128398fdbf010a950f720008a62
Matthew Macovsky <macovsky.m@gmail.com>
2015-10-13 20:01:54 -0700

    Made logger work on the room computer, added in logging of input from the serial port, and created tentative list of people to be integrated into the new system when the new tags arrive.

--- logger.cpp
+++ logger.cpp
@@ -7,6 +7,7 @@
 #include <ctime>
 #include <cassert>
 #include <sstream>
+#include <algorithm>
 
 #include "rfid.h"
 
@@ -47,7 +48,7 @@ void logAttendance(std::string name) {
 	Time time = getTime();
 	std::string formatted_time = formatTime(time);
 	std::ofstream log("log.txt", std::ofstream::app);
-	log<<"\n"<<name;
+	log<<"\r\n"<<name;
 	for (int i = name.size(); i < NAME_SPACE; i++) log<<" ";
 	log<<"="<<" "<<formatted_time;
 	log.close();
@@ -64,6 +65,7 @@ std::string getName(std::string uid) {
 	}
 	ids_and_names.close();
 	std::string name = line.substr(line.find("=") + 2);
+	name.erase(std::remove(name.begin(), name.end(), '\r'), name.end());
 	return name;
 }
 
@@ -85,6 +87,8 @@ bool checkUID(std::string uid) {
 }
 
 int main(int argc, char* argv[]) {
+	bool logging = false; //In config file eventually
+	
 	bool quitting = false;
 	
 	std::string no_arduino_str = "-noarduino";
@@ -100,7 +104,7 @@ int main(int argc, char* argv[]) {
 
 		HANDLE hCom;
 		hCom = CreateFile(
-			TEXT("\\\\.\\COM3"),          //Change to whatever COM port the Arduino is on
+			TEXT("\\\\.\\COM4"),          //Change to whatever COM port the Arduino is on
 			GENERIC_READ | GENERIC_WRITE,
 			0,                            // exclusive access 
 			NULL,                         // default security attributes 
@@ -121,88 +125,99 @@ int main(int argc, char* argv[]) {
 			char ch_buffer[1024] = "";
 			DWORD dw_bytes_read = 0;
 			
-			WaitCommEvent(hCom, &dw_evt_mask, NULL);
+			bool comm_event_sucess = WaitCommEvent(hCom, &dw_evt_mask, NULL);
+			Sleep(250);
 			
 			ReadFile(hCom, &ch_buffer, 8, &dw_bytes_read, NULL);
 			
 			std::string uid(ch_buffer);
 			uid = uid.substr(0, 8);
 			
-			if (uid == "FFFFFFFF") {
-				std::cout<<"Please enter either your first and last name or \"quit\" to quit."<<std::endl;
-				
-				bool timed_out = false;
-				char buffer[256] = {0};
-				data arg = {buffer, 256};
-				HANDLE hThread; 
-				DWORD id;
-				std::string name;
-
-				hThread = CreateThread(NULL, 0, getInput, &arg, 0, &id);
-
-				if (hThread != NULL) {
-					if (WaitForSingleObjectEx(hThread, MANUAL_ENTRY_TIME, FALSE) == WAIT_TIMEOUT) {
-						std::cout<<"User timed out."<<std::endl;
-						timed_out = true;
-						/*std::streambuf* orig = std::cin.rdbuf();
-						std::istringstream input("\n");
-						std::cin.rdbuf(input.rdbuf());
-						std::cin.rdbuf(orig);*/
-					} else {
-						name = buffer;
-					}
-					CloseHandle(hThread);
-				}
-				
-				if (!timed_out) {
-					if (name != "quit") {
-						//std::cout<<std::endl<<"NAME: "<<name<<std::endl;
-						/*for (unsigned int i = 0; i < name.size(); i++) {
-							if (name[i] == '\n') name.erase(i, 1);
-						}*/
-						bool valid_name = checkName(&name);
-						if (valid_name) {
-							logAttendance(name);
-							led_mode = Led_mode::LOGGED;
-						} else {
-							std::cout<<"Invalid entry.  Either the name was entered incorrectly or no such name is stored in the system."<<std::endl;
-							led_mode = Led_mode::LOGGING_ERROR;
-						}
-					} else {
-						quitting = true;
-						led_mode = Led_mode::QUITTING;
-					}
-				} else {
-					led_mode = Led_mode::LOGGING_ERROR;
-				}
-			} else {
-				bool valid_uid = checkUID(uid);
-				if (valid_uid) {
-					std::string name = getName(uid);
-					logAttendance(name);
-					led_mode = Led_mode::LOGGED;
-				} else {
-					std::cout<<"Invalid tag.  Either your tag is damaged or no such tag is stored in the system."<<std::endl;
-					led_mode = Led_mode::LOGGING_ERROR;
-				}
+			if (logging) {
+				std::ofstream serial_logging_file;
+				serial_logging_file.open("serial_log.txt", std::fstream::app);
+				serial_logging_file<<ch_buffer<<std::endl;
+				serial_logging_file.close();
 			}
 			
-			DWORD dw_bytes_written = 0;
-			char to_write[2] = {[&]() {
-				switch (led_mode) {
-					case Led_mode::LOGGED:
-						return '0';
-					case Led_mode::LOGGING_ERROR:
-						return '1';
-					case Led_mode::QUITTING:
-						return '2';
-					default:
-						assert(0);
+			if (uid.size() > 0) {
+				if (uid == "FFFFFFFF") {
+					std::cout<<"Please enter either your first and last name or \"quit\" to quit."<<std::endl;
+					
+					bool timed_out = false;
+					char buffer[256] = {0};
+					data arg = {buffer, 256};
+					HANDLE hThread; 
+					DWORD id;
+					std::string name;
+
+					hThread = CreateThread(NULL, 0, getInput, &arg, 0, &id);
+
+					if (hThread != NULL) {
+						if (WaitForSingleObjectEx(hThread, MANUAL_ENTRY_TIME, FALSE) == WAIT_TIMEOUT) {
+							std::cout<<"User timed out."<<std::endl;
+							timed_out = true;
+							/*std::streambuf* orig = std::cin.rdbuf();
+							std::istringstream input("\n");
+							std::cin.rdbuf(input.rdbuf());
+							std::cin.rdbuf(orig);*/
+						} else {
+							name = buffer;
+						}
+					}
+					
+					CloseHandle(hThread);
+					
+					if (!timed_out) {
+						if (name != "quit") {
+							//std::cout<<std::endl<<"NAME: "<<name<<std::endl;
+							/*for (unsigned int i = 0; i < name.size(); i++) {
+								if (name[i] == '\n') name.erase(i, 1);
+							}*/
+							bool valid_name = checkName(&name);
+							if (valid_name) {
+								logAttendance(name);
+								led_mode = Led_mode::LOGGED;
+							} else {
+								std::cout<<"Invalid entry.  Either the name was entered incorrectly or no such name is stored in the system."<<std::endl;
+								led_mode = Led_mode::LOGGING_ERROR;
+							}
+						} else {
+							quitting = true;
+							led_mode = Led_mode::QUITTING;
+						}
+					} else {
+						led_mode = Led_mode::LOGGING_ERROR;
+					}
+				} else {
+					bool valid_uid = checkUID(uid);
+					if (valid_uid) {
+						std::string name = getName(uid);
+						logAttendance(name);
+						led_mode = Led_mode::LOGGED;
+					} else {
+						std::cout<<"Invalid tag.  Either your tag is damaged or no such tag is stored in the system."<<std::endl;
+						led_mode = Led_mode::LOGGING_ERROR;
+					}
 				}
-				return '3';
-			}()};
-			
-			WriteFile(hCom, to_write, 1, &dw_bytes_written, NULL);
+				
+				DWORD dw_bytes_written = 0;
+				char to_write[2] = {[&]() {
+					switch (led_mode) {
+						case Led_mode::LOGGED:
+							return '0';
+						case Led_mode::LOGGING_ERROR:
+							return '1';
+						case Led_mode::QUITTING:
+							return '2';
+						default:
+							assert(0);
+					}
+					return '3';
+				}()};
+				
+				WriteFile(hCom, to_write, 1, &dw_bytes_written, NULL);
+			}
 		}
 		CloseHandle(hCom);
 	} else {
